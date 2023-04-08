4debb12d28d78b22975e954afd7c70379c0e3c95
vickyylopezz <vlopez@fi.uba.ar>
2023-04-08 19:57:22 -0300

    add: creacion de Pedido

---
+++ src/main.rs
@@ -0,0 +1,77 @@
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader, Error},
+};
+
+use pedido::Pedido;
+mod pedido;
+
+/// Abre el archivo del path recibido por parametro y si falla vuelve a pedirlo.
+/// Ejemplo:
+/// ```rust
+/// abrir_archivo(nombreArchivo);
+/// ```
+fn abrir_archivo(path: String) -> File {
+    match File::open(path) {
+        Ok(it) => it,
+        Err(_) => {
+            println!("Nombre de archivo incorrecto. Por favor vuelva a ingresarlo:");
+            abrir_archivo(leer_por_pantalla())
+        }
+    }
+}
+
+/// Lee las lineas del archivo recibido por parametro, las guarda en un vector y lo devuelve.
+/// Ejemplo:
+/// ```rust
+/// read_file_lines(nombreArchivo);
+/// ```
+fn read_file_lines(path: String) -> Result<Vec<Vec<i32>>, Error> {
+    let mut vector = Vec::new();
+    let reader = BufReader::new(abrir_archivo(path));
+    for line in reader.lines() {
+        let pedido: Vec<i32> = line?
+            .split(',')
+            .map(|x| x.parse().expect("Failed to read file"))
+            .collect();
+        vector.push(pedido);
+    }
+
+    Ok(vector)
+}
+
+/// Lee por pantalla, guarda lo leido en una variable y la devuelve.
+/// Ejemplo:
+/// ```rust
+/// leer_por_pantall();
+/// ```
+fn leer_por_pantalla() -> String {
+    let mut archivo_ingresado = String::new();
+
+    io::stdin()
+        .read_line(&mut archivo_ingresado)
+        .expect("Failed to read line");
+
+    archivo_ingresado.trim().to_string()
+}
+
+fn pedidos(pedidos_archivo: Vec<Vec<i32>>) -> Vec<Pedido> {
+    let mut pedidos = Vec::<Pedido>::new();
+    for pedido in pedidos_archivo {
+        pedidos.push(Pedido {
+            cafe_molido: pedido[0],
+            agua_caliente: pedido[1],
+            cacao: pedido[2],
+            espuma: pedido[3],
+        })
+    }
+    pedidos
+}
+
+fn main() {
+    println!("Bienvenido!");
+    println!("Ingrese el archivo con el pedido");
+    let pedidos_archivo = read_file_lines(leer_por_pantalla()).expect("Failed to read file");
+    let pedidos = pedidos(pedidos_archivo);
+    println!("{:?}", pedidos)
+}
---
+++ src/pedido.rs
@@ -0,0 +1,12 @@
+#[derive(Debug)]
+pub struct Pedido {
+    /// Cantidad de cafe molido
+    pub cafe_molido: i32,
+    /// Cantidad de agua caliente
+    pub agua_caliente: i32,
+    /// Cantidad de cacao
+    pub cacao: i32,
+    /// Cantidad de espuma
+    pub espuma: i32,
+    //pub id: i32?
+}
